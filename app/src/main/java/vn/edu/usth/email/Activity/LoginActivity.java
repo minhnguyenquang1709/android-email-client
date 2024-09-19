097caefe5f13b2d8e6c591baa70538d54256b83d
MinhNQ <minhnguyenquang2004vn@gmail.com>
2024-09-19 20:54:18 +0700

    add super basic login

---
+++ app/src/main/java/vn/edu/usth/email/Activity/LoginActivity.java
@@ -0,0 +1,55 @@
+package vn.edu.usth.email.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import vn.edu.usth.email.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextInputEditText emailInputField;
+    private TextInputEditText passwordInputField;
+    private String email;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        emailInputField = findViewById(R.id.emailSender);
+        passwordInputField = findViewById(R.id.password);
+
+
+
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = emailInputField.getText().toString();
+                password = passwordInputField.getText().toString();
+
+                // check account
+                if (!email.equals("test@gmail.com")  || !password.equals("12345678")){
+                    Toast.makeText(getApplicationContext(), "Incorrect email or password", Toast.LENGTH_SHORT).show();
+                }else{
+                    Log.i("LoginActivity", email + " - " + password);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+}
