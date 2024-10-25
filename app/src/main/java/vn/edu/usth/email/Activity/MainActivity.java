652c91aee5f58040118079c461f4b1d24c191211
Nguyen Thanh Long <longnt,22bi13264@usth.edu.vn>
2024-10-25 07:56:50 +0700

    gen setting

--- app/src/main/java/vn/edu/usth/email/Activity/MainActivity.java
+++ app/src/main/java/vn/edu/usth/email/Activity/MainActivity.java
@@ -78,7 +78,7 @@ public class    MainActivity extends AppCompatActivity {
         settingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                Intent intent = new Intent(MainActivity.this, GeneralSettingActivity.class);
                 startActivity(intent);
             }
         });
