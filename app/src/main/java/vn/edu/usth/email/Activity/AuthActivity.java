b43d5b01602bd1b94c89c17e52087dde05a06299
MinhNQ <minhnguyenquang2004vn@gmail.com>
2024-11-09 04:24:55 +0700

    remove fetch email messages (move to other Activities)

--- app/src/main/java/vn/edu/usth/email/Activity/AuthActivity.java
+++ app/src/main/java/vn/edu/usth/email/Activity/AuthActivity.java
@@ -1,14 +1,13 @@
 package vn.edu.usth.email.Activity;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.credentials.Credential;
 //import com.google.api.client.auth.oauth2.Credential;
 import androidx.credentials.CredentialManager;
 
 import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.IntentSender;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CancellationSignal;
 import android.os.Handler;
@@ -28,8 +27,6 @@ import androidx.credentials.CredentialManagerCallback;
 import androidx.credentials.GetCredentialRequest;
 import androidx.credentials.GetCredentialResponse;
 import androidx.credentials.GetPasswordOption;
-import androidx.credentials.PasswordCredential;
-import androidx.credentials.PublicKeyCredential;
 import androidx.credentials.exceptions.GetCredentialException;
 
 import com.google.android.gms.auth.api.identity.AuthorizationRequest;
@@ -38,22 +35,10 @@ import com.google.android.gms.common.api.Scope;
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption;
 import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption;
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.ListMessagesResponse;
-import com.google.api.services.gmail.model.Message;
-import com.google.api.services.gmail.model.MessagePart;
-import com.google.api.services.gmail.model.MessagePartBody;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -68,7 +53,20 @@ public class AuthActivity extends AppCompatActivity {
 
     private ArrayList<String> accessTokenList;
 
-    private Gmail service;
+    private final Handler handler;
+
+    public AuthActivity() {
+        Log.i("AuthActivity", "AuthActivity constructor");
+        this.handler = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(@NonNull android.os.Message msg) {
+                // executed in the main thread
+                String content = msg.getData().toString();
+                Toast.makeText(AuthActivity.this, content, Toast.LENGTH_SHORT).show();
+                Log.i("Handler", content);
+            }
+        };;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,16 +83,6 @@ public class AuthActivity extends AppCompatActivity {
 
         accessTokenList = new ArrayList<>();
 
-        final Handler handler = new Handler(Looper.getMainLooper()){
-            @Override
-            public void handleMessage(@NonNull android.os.Message msg) {
-                // executed in the main thread
-                String content = msg.getData().toString();
-                Toast.makeText(AuthActivity.this, content, Toast.LENGTH_SHORT).show();
-                Log.i("Handler", content);
-            }
-        };
-
         // initialize a CredentialManager object
         CredentialManager credentialManager = CredentialManager.create(this);
 
@@ -110,7 +98,6 @@ public class AuthActivity extends AppCompatActivity {
             .setNonce(nonce)
             .build();
 
-        // this must be the only option in GetCredentialRequest
         // retrieve user's Google ID Token
         GetSignInWithGoogleOption getSignInWithGoogleOption = new GetSignInWithGoogleOption.Builder(getString(R.string.client_id))
             .setNonce(nonce)
@@ -119,16 +106,6 @@ public class AuthActivity extends AppCompatActivity {
         GetCredentialRequest getCredRequest = new GetCredentialRequest.Builder()
             .addCredentialOption(getSignInWithGoogleOption)
             .build();
-
-        // build a new authorized API client service
-//        try {
-//            initializeGmailApiService();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        } catch (GeneralSecurityException e) {
-//            throw new RuntimeException(e);
-//        }
-
 
         // click on the view -> start authentication
         boxAddAddress = findViewById(R.id.box_add_address);
@@ -162,70 +139,89 @@ public class AuthActivity extends AppCompatActivity {
         });
     }
 
-//    private void initializeGmailApiService() throws IOException, GeneralSecurityException{
-//        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-//        JSON_FACTORY = GsonFactory.getDefaultInstance();
-//        service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, )
-//    }
-
     // Start the Google Sign-In intent
     private void handleSignIn(GetCredentialResponse result) {
         // Handle the successfully returned credential.
         Credential credential = result.getCredential();
-        if (credential instanceof PublicKeyCredential) {
-            String responseJson = ((PublicKeyCredential) credential).getAuthenticationResponseJson();
-            Toast.makeText(this, "PublicKeyCredential received: " + responseJson, Toast.LENGTH_LONG).show();
-            // Share responseJson i.e. a GetCredentialResponse on your server to validate and authenticate
-        } else if (credential instanceof PasswordCredential) {
-            String username = ((PasswordCredential) credential).getId();
-            String password = ((PasswordCredential) credential).getPassword();
-            Toast.makeText(this, "Username: " + username + ", Password: " + password, Toast.LENGTH_LONG).show();
-            // Use id and password to send to your server to validate and authenticate
-        } else {
-            // Catch any unrecognized credential type here.
-            Log.i("AuthActivity", "Unexpected type of credential: " + result.toString());
-//            Toast.makeText(this, "Unexpected type of credential", Toast.LENGTH_SHORT).show(); // must call Looper.prepare()
-        }
 
-//        Log.i("AuthActivity", "credential: "+credential.getType());
+//        if (credential instanceof PublicKeyCredential) {
+//            String responseJson = ((PublicKeyCredential) credential).getAuthenticationResponseJson();
+//            Toast.makeText(this, "PublicKeyCredential received: " + responseJson, Toast.LENGTH_LONG).show();
+//        } else if (credential instanceof PasswordCredential) {
+//            String username = ((PasswordCredential) credential).getId();
+//            String password = ((PasswordCredential) credential).getPassword();
+//            Toast.makeText(this, "Username: " + username + ", Password: " + password, Toast.LENGTH_LONG).show();
+//        } else {
+//            // Catch any unrecognized credential type here.
+//            Log.i("AuthActivity", "Unexpected type of credential: " + result.toString());
+//
+//        }
+
         if(credential.getType().equals(GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL)){
             // convert the credential object into a GoogleIdTokenCredential
             GoogleIdTokenCredential idTokenCredential = GoogleIdTokenCredential.createFrom(credential.getData());
             // extract GoogleIdTokenCredential
-//            Log.i("AuthActivity", "id: " + idTokenCredential.getId()); // the email
-//            Log.i("AuthActivity", "idToken: " + idTokenCredential.getIdToken()); // JWT token
+            // Log.i("AuthActivity", "id: " + idTokenCredential.getId()); // the email
+            // Log.i("AuthActivity", "idToken: " + idTokenCredential.getIdToken()); // JWT token
 
             credentialList.add(idTokenCredential);
-
-            runOnUiThread(()-> updateEmailList());
         }
     }
 
     // update UI
     private void updateEmailList(){
         LinearLayout container = findViewById(R.id.email_list);
-        credentialList.forEach(credential->{
+        container.removeAllViews();
+        for(int i = 0; i < credentialList.size(); i++){
             TextView tv = new TextView(AuthActivity.this);
             tv.setWidth(container.getWidth());
-            tv.setText(credential.getId());
+            tv.setText(credentialList.get(i).getId());
             tv.setTextColor(getColor(R.color.blue_text));
             container.addView(tv);
-        });
+
+            String accessToken = null;
+
+            try {
+                accessToken = accessTokenList.get(i);
+            }catch (IndexOutOfBoundsException e){
+                Log.e("Auth Error", "No corresponding access token");
+            }
+
+            if(accessToken != null){
+                String accessTok = accessToken; // for calling from inner class
+                tv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String email = tv.getText().toString();
+                        Intent intent = new Intent(AuthActivity.this, HomeActivity.class);
+                        intent.putExtra("userId", email);
+                        intent.putExtra("accessToken", accessTok);
+                        startActivity(intent);
+                    }
+                });
+            }else{
+                // if unable to get access token, run authorization again when pressing on email
+                tv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String email = tv.getText().toString();
+                        authorize(handler);
+                    }
+                });
+            }
+        }
     }
 
     // Handle errors during credential retrieval
     private void handleFailure(GetCredentialException e) {
-        //Toast.makeText(this, "Credential retrieval failed: " + e.getMessage(), Toast.LENGTH_LONG).show();
         Log.e("AuthActivtiy Error", "Credential retrieval failed: " + e);
-        // Implement fallback logic: show a sign-in form
     }
 
     // authorize google user
-    private void authorize(Handler handler){
+    private void  authorize(Handler handler){
         List<Scope> requestedScopes = Arrays.asList(
-                new Scope(GmailScopes.GMAIL_READONLY),
                 new Scope(GmailScopes.GMAIL_LABELS),
-                new Scope(GmailScopes.GMAIL_COMPOSE) );
+                new Scope(GmailScopes.MAIL_GOOGLE_COM));
         AuthorizationRequest authorizationRequest = AuthorizationRequest.builder()
                 .setRequestedScopes(requestedScopes)
                 .build();
@@ -243,145 +239,21 @@ public class AuthActivity extends AppCompatActivity {
                                 }catch (IntentSender.SendIntentException e){
                                     Log.e("AuthActivity Error", "Couldn't start Authorization UI: " + e.getLocalizedMessage());
                                 }
+                                runOnUiThread(()-> updateEmailList());
                             }else{
-                                // access already granted, continue with user action
-                                // logic to get data
+                                // access already granted, get access token
                                 String accessToken = authorizationResult.getAccessToken();
                                 Log.i("AuthActivity", "Authorization successful");
                                 Log.i("AuthActivity", "accessToken: " + accessToken);
                                 accessTokenList.add(accessToken);
-//                                try {
-//                                    service = initializeGmailApiService(accessTokenList.get(0));
-//                                } catch (GeneralSecurityException e) {
-//                                    throw new RuntimeException(e);
-//                                } catch (IOException e) {
-//                                    throw new RuntimeException(e);
-//                                }
-                            }
-
-//                            Credential credential = credentialList.get(0);
-//                            Log.i("Get Credential", credential.toString());
-
-                            // get userId from Userinfo API
-//                            HttpTransport httpTransport = new NetHttpTransport();
-//                            HttpRequestFactory requestFactory = httpTransport.createRequestFactory(
-//                                    request -> request.getHeaders().setAuthorization("Bearer " + accessToken)
-//                            );
-
-                            String accessToken = accessTokenList.get(0);
-                            try {
-                                service = initializeGmailApiService(accessToken);
-                                Log.i("Gmail", "Initialized Gmail service instance");
-
-                                if(service == null){
-                                    Log.i("Gmail Service Instance", "No service instance");
-                                }else{
-                                    GoogleIdTokenCredential credential = credentialList.get(0);
-                                    String userId = credential.getId();
-                                    fetchEmailsMessages(userId, service, handler);
-                                }
-                            } catch (GeneralSecurityException e) {
-                                throw new RuntimeException(e);
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
+                                runOnUiThread(()-> updateEmailList());
                             }
                         }
                 )
                 .addOnFailureListener(e -> Log.e("AuthActivity", "Failed to authorize", e));
     }
 
-    // request email data
-    private Gmail initializeGmailApiService(String accessToken) throws GeneralSecurityException, IOException {
-        Log.i("GmailAPI", "start getting email data...");
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_READONLY);
-        final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();;
 
-        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, request -> {
-            request.setInterceptor(httpRequest->{
-                // set the access token in the request header
-                httpRequest.getHeaders().setAuthorization("Bearer " + accessToken);
-            });
-        }).build();
 
-        return service;
-    }
 
-    private void fetchEmailsMessages(String userId, Gmail service, Handler handler){
-        Log.i("fetching", "start getting email messages");
-        Thread bgThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // get email messages
-                ListMessagesResponse listResponse = null;
-                try {
-                    listResponse = service.users().messages().list(userId).execute();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                List<Message> messages = listResponse.getMessages();
-
-                if(messages == null || messages.isEmpty()){
-                    Log.i("GmailAPI", "No messages found.");
-                }else{
-                    Log.i("GmailAPI", "Messages:");
-
-                    for (Message message: messages){
-                        Message fullMessage = null;
-                        try {
-                            fullMessage = service.users().messages().get(userId, message.getId()).execute();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                        // get the payload
-                        MessagePart payload = fullMessage.getPayload();
-
-                        if (payload != null){
-                            // get the parts of the message (if it's multipart)
-                            List<MessagePart> parts = payload.getParts();
-
-                            // get the message body
-                            if (parts == null || parts.isEmpty()) {
-                                // Get the body of the message directly
-                                MessagePartBody body = payload.getBody();
-                                if (body != null){
-                                    String messageBody = new String(payload.getBody().decodeData());
-                                    Log.i("Email message","Message Body: " + messageBody);
-                                } else{
-                                    Log.e("GmailAPI", "Message body is null.");
-                                }
-                            } else {
-                                // If the message is multipart, iterate through the parts
-                                for (MessagePart part : parts) {
-                                    if (part != null){
-                                        // Check if the part is text/plain or text/html
-                                        if (part.getMimeType().equals("text/plain")) {
-                                            String body = new String(part.getBody().decodeData());
-                                            Log.i("Email message", "Plain Text Body: " + body);
-                                        } else if (part.getMimeType().equals("text/html")) {
-                                            String htmlBody = new String(part.getBody().decodeData());
-                                            Log.i("Email message", "HTML Body: " + htmlBody);
-                                        }
-                                    } else{
-                                        Log.e("GmailAPI", "Part body is null for part: ");
-                                    }
-                                }
-                            }
-                        } else{
-                            Log.e("GmailAPI", "No payload");
-//                            Log.i("GmailAPI", message.decodeRaw());
-                        }
-                        break;
-                    }
-
-                    Bundle bundle = new Bundle();
-                    bundle.putString("server_response", "check logcat");
-                    android.os.Message msg = new android.os.Message();
-                    msg.setData(bundle);
-                    handler.sendMessage(msg);
-                }
-            }
-        });
-        bgThread.start();
-    }
 }
